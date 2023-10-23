20b88f9e2d158ef3e1ddac491f9ae0f86030b75f
vts1978 <vinicioussouza@gmail.com>
2023-10-23 19:05:49 -0300

    Resolucao teste unitarios

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -45,10 +45,15 @@ public class ValidacoesListaTests
 
         // Act
 
+        var result = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
+
         // Assert
+
+        Assert.False(result);
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
@@ -58,8 +63,10 @@ public class ValidacoesListaTests
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var result = _validacoes.MultiplicarNumerosLista(lista, 2);
 
         // Assert
+        Assert.Equal(resultadoEsperado, result);
     }
 
     [Fact]
@@ -71,10 +78,11 @@ public class ValidacoesListaTests
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        var result = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(9, result);
     }
 
     [Fact]
@@ -90,6 +98,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
